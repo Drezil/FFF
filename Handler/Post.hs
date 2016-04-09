7ca9c2300cdc01a286e7cbff870127558267e9fd
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2016-04-09 21:06:51 +0200

    initial commit.

---
+++ Handler/Post.hs
@@ -0,0 +1,9 @@
+module Handler.Post where
+
+import Import
+
+getPostR :: Handler Html
+getPostR = error "Not yet implemented: getPostR"
+
+postPostR :: Handler Html
+postPostR = error "Not yet implemented: postPostR"
---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "fff" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
